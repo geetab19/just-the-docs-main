7d86bae3029142bb2313338bbd79ba2b20b7ed68
Geeta <geeta@skybin.io>
2023-03-09 16:14:17 +0530

    fixes

---
+++ .tina/config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "df8f0f35-dc4b-426b-ae0d-9241538e77c5", // Get this from tina.io
+  token: "3c507850d27e957657f3f317999385f6570b15a4", // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "./",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "./",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
