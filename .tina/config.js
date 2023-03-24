7f176eb319abfd0da8e9c65180ef6eb7fc299b3d
Geeta <togeeta.bisht19@gmail.com>
2023-03-24 18:34:13 +0530

    Update config.js

--- .tina/config.js
+++ .tina/config.js
@@ -8,13 +8,13 @@ export default defineConfig({
   clientId: "611c6f3d-4132-4636-ab81-24482bc7713a", // Get this from tina.io
   token: "bcdb3b7397a9321b26d979c111a55cd57f10fabd", // Get this from tina.io
   build: {
-    outputFolder: "just-the-docs-main/admin",
-    publicFolder: "public",
+     outputFolder: "just-the-docs-main/admin",
+     publicFolder: "just-the-docs-main/",
   },
   media: {
     tina: {
-      mediaRoot: "public",
-      publicFolder: "uploads",
+      mediaRoot: "just-the-docs-main/uploads",
+      publicFolder: "just-the-docs-main/assets",
     },
   },
   schema: {
