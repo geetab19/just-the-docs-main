328b17c945dcb9c3948fde2a8414e1a62fa22db3
Geeta <geeta@skybin.io>
2023-03-26 22:55:52 +0530

    fix

--- .tina/config.js
+++ .tina/config.js
@@ -1,20 +1,20 @@
 import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master";
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
   clientId: "611c6f3d-4132-4636-ab81-24482bc7713a", // Get this from tina.io
   token: "bcdb3b7397a9321b26d979c111a55cd57f10fabd", // Get this from tina.io
   build: {
-    outputFolder: "just-the-docs-main/admin",
-    publicFolder: "just-the-docs-main/",
+    outputFolder: "admin",
+    publicFolder: "/",
   },
   media: {
     tina: {
-      mediaRoot: "just-the-docs-main/uploads",
-      publicFolder: "just-the-docs-main/assets",
+      mediaRoot: "uploads",
+      publicFolder: "/",
     },
   },
   schema: {
@@ -69,9 +69,9 @@ export default defineConfig({
             isBody: true,
           },
           {
-            name: 'draft',
-            label: 'Draft',
             type: 'boolean',
+            name: 'draft',
+            label: 'Draft',          
             required: true,
             description: 'If this is checked the post will not be published',
           },
