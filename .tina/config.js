c12deaebb2bad2d8b7afde21a3ced58a1f1691ee
togeeta.bisht19@gmail.com <togeeta.bisht19@gmail.com>
2023-04-01 01:08:12 +0530

    Updated Config and 183 other files

---
+++ .tina/config.js
@@ -0,0 +1,82 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master";
+
+export default defineConfig({
+  branch,
+  clientId: "611c6f3d-4132-4636-ab81-24482bc7713a", // Get this from tina.io
+  token: "bcdb3b7397a9321b26d979c111a55cd57f10fabd", // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "/",
+  },
+  media: {
+    tina: {
+      mediaRoot: "uploads",
+      publicFolder: "assets",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        format: 'md',
+        ui: {
+          filename: {
+            readonly: false,
+            slugify: values => {
+              const date = new Date();
+              const day = date.getDate();
+              const month = date.getMonth() + 1;
+              const year = date.getFullYear();        
+              let currentDate = `${year}-${month}-${day}`;        
+              return `${currentDate}-${values?.title?.toLowerCase().replace(/ /g, '-')}`
+            }
+          }
+        },
+        fields: [        
+          {
+            type: "string",
+            name: "layout",
+            label: "Layout",
+            required: false,
+          },
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          { 
+            type: "datetime",
+            name: "date",
+            label: "Date",
+            required: true,
+          },
+          { 
+            type: "string",
+            name: "categories",
+            label: "Categories",
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+          {
+            type: 'boolean',
+            name: 'draft',
+            label: 'Draft',          
+            required: true,
+            description: 'If this is checked the post will not be published',
+          },
+        ],
+      },
+    ],
+  },
+});
---
+++ content/posts/2023-3-28-test-new-layout.md
@@ -0,0 +1,11 @@
+---
+title: test new Layout
+date: 2023-03-27 18:30:00 Z
+layout: newlayout
+categories: test Cat
+draft: false
+---
+
+In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. [Wikipedia](https://en.wikipedia.org/wiki/Lorem_ipsum)
+
+[https://assets.tina.io/611c6f3d-4132-4636-ab81-24482bc7713a/test-21.jpg](https://assets.tina.io/611c6f3d-4132-4636-ab81-24482bc7713a/test-21.jpg)
