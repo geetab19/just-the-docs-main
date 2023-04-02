fb1fb46a807bb742796126b41067c7be4ed5db02
Geeta <geeta@skybin.io>
2023-04-02 23:50:43 +0530

    deleyed tinsa

--- .tina/__generated__/client.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'https://content.tinajs.io/1.4/content/611c6f3d-4132-4636-ab81-24482bc7713a/github/master', token: 'bcdb3b7397a9321b26d979c111a55cd57f10fabd', queries });
-export default client;
-  
